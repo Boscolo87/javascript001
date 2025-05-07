5442929cf18b26d7af08feea42693968d3e8cc29
Boscolo87 <joelboscolo3@gmail.com>
2025-05-06 22:02:59 -0300

    declaração de variável

---
+++ app01.js
@@ -0,0 +1,38 @@
+// Estou começando a estudar programação . Estou começando com a linguagem javascript. Elenque os principais tópoicos que um inciante precisa estudar sobre javascript.
+ 
+// Boas práticas
+// Case sensitive
+// Sintaxe básica e comentário
+// Declaração de variáveis(var, let, const)
+let nome ="joel"
+let idade = 37
+
+// Console e Debug
+
+// Operadores
+//     Operador Atribuição
+//     Operadores aritméticos
+//     Operadores relacionais
+//     Operadores lógicos
+// Estrutura
+//     Estrutura de controle/decisão
+//     Laços de repetição
+// Arrays 
+// Funções básicas
+// Escopo
+
+// Alterar conteúdo e atributos *
+// incluir js em página html *
+// Eventos *
+// DOM *
+// Objeto
+// parâmetros e retorno*
+
+// Programação assíncrona***
+// API Fetch ***
+
+// JSON ****
+// funções modernas (arrow functions) ****
+
+// Spread e REST  ************
+ 
